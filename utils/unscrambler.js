14014e6682b4e1359672871238c111555a42e250
Rahul Hoque <rhoque@umich.edu>
2024-06-12 10:33:33 -0400

    fix api

--- utils/unscrambler.js
+++ utils/unscrambler.js
@@ -23,7 +23,7 @@ async function getCachedWordList(filePath) {
 
 async function isValidScrabbleWord(word) {
     try {
-        const wordList = await getCachedWordList('./public/twl.txt');
+        const wordList = await getCachedWordList('/vercel/path0/public/twl.txt'); // Use absolute path
         return wordList.has(word.toLowerCase());
     } catch (error) {
         console.error('Error loading word list:', error);
